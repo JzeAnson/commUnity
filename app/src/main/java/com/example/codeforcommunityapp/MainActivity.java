857fd03409af12dfb7b2ef11761ddd762e899eb2
Jonathan Siew <jonathansiew.2004@gmail.com>
2024-12-07 16:07:55 +0800

    Detecting Changes in Another Branch

--- app/src/main/java/com/example/codeforcommunityapp/MainActivity.java
+++ app/src/main/java/com/example/codeforcommunityapp/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     //Change 1
+    //Change 2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
