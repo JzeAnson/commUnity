18a311ee7d70b911550fac9e1c9491735b6a2ea4
jze0725 <jze0725@gmail.com>
2024-12-20 15:37:26 +0800

    finished project

---
+++ app/src/main/java/com/example/community/HomeFragment.java
@@ -0,0 +1,32 @@
+package com.example.community;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+
+
+    }
+}
---
+++ app/src/main/java/com/example/community/HomeMain.java
@@ -0,0 +1,64 @@
+package com.example.community;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.community.databinding.ActivityHomeMainBinding;
+
+public class HomeMain extends AppCompatActivity {
+
+    ActivityHomeMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityHomeMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        String documentID = userDocument.getInstance().getDocumentId();
+        //Toast.makeText(getApplicationContext(),documentID,Toast.LENGTH_LONG).show();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            int itemId = item.getItemId();
+            if (itemId == R.id.home) {
+                replaceFragment(new HomeFragment());
+                return true;
+
+            } else if (itemId == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+                return true;
+
+            } else if (itemId == R.id.setting) {
+                replaceFragment(new SettingFragment());
+                return true;
+
+            }
+
+            return false;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+}
