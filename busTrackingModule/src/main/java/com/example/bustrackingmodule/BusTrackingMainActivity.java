c8d79c45292211b7eb686efabbd9ce24ddc868f7
jianwen0414 <jianwen0414@gmail.com>
2024-12-26 01:49:35 +0800

    Commit

---
+++ busTrackingModule/src/main/java/com/example/bustrackingmodule/BusTrackingMainActivity.java
@@ -0,0 +1,33 @@
+package com.example.bustrackingmodule;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.play.core.splitcompat.SplitCompat;
+
+public class BusTrackingMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_bus_tracking_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase){
+        super.attachBaseContext(newBase);
+        SplitCompat.install(this);
+    }
+}
