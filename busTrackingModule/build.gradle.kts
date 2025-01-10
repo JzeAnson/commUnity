a4f9f92ca4e1a27c84a4273c4f8627d6f67ca707
Jonathan Siew <jonathansiew.2004@gmail.com>
2025-01-10 11:32:17 +0800

    Fixing App Crashing Issue (Part 15)

--- busTrackingModule/build.gradle.kts
+++ busTrackingModule/build.gradle.kts
@@ -11,6 +11,11 @@ android {
         minSdk = 26  // Match with app module
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
 }
 
 dependencies {
@@ -19,21 +24,24 @@ dependencies {
         exclude("com.google.android.play", "core-common")
     }
     // AndroidX and Material
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.activity:activity:1.8.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.5.3")
     implementation("androidx.navigation:navigation-ui:2.5.3")
+    implementation("androidx.fragment:fragment:1.6.1")
+    implementation("androidx.activity:activity:1.8.0")
+
+
+
 
     // Maps and Location
-    implementation("com.google.android.gms:play-services-maps:18.2.0")
-    implementation("com.google.maps.android:android-maps-utils:3.4.0")
+
+    implementation(project(":app"))
+
 
     // Network
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("org.mobilitydata:gtfs-realtime-bindings:0.0.8")
+    implementation(libs.fragment)
 
     // Testing
     testImplementation("junit:junit:4.13.2")
